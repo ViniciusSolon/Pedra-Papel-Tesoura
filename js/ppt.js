366e71a923b126abcdc1bb2f6a5b2d0ca4f9f7aa
Vinicius Solon <viniciussolonc@gmail.com>
2022-08-06 22:53:36 -0300

    first commit

---
+++ js/ppt.js
@@ -0,0 +1,102 @@
+var elementos = document.querySelectorAll('.player-options div > img');
+	var playerOpt = "";
+	var inimigoOpt = "";
+	
+
+
+	function resetInimigo() {
+		const enemyOptions = document.querySelectorAll('.enemy-options div');
+		for(var i= 0; i < enemyOptions.length; i++){
+			
+				enemyOptions[i].childNodes[0].style.opacity = 0.3;
+			}
+		
+	}
+
+
+	function inimigoJogar(){
+		let rand = Math.floor(Math.random()*3);
+
+		const enemyOptions = document.querySelectorAll('.enemy-options div');
+		resetInimigo();
+		for(var i= 0; i < enemyOptions.length; i++){
+			if(i == rand){
+				enemyOptions[i].childNodes[0].style.opacity = 1;
+				inimigoOpt = enemyOptions[i].childNodes[0].getAttribute('opt');
+			}
+		}
+		//alert(playerOpt);
+		//alert(inimigoOpt);
+		validarVitoria();
+
+	}
+
+	function resetOpacityPlayer(){
+		for(var i = 0; i < elementos.length; i++){
+		elementos[i].style.opacity = 0.3;
+	}
+	}
+
+	
+
+	for(var i = 0; i < elementos.length; i++){
+		elementos[i].addEventListener('click',function(t){
+			resetOpacityPlayer();
+			t.target.style.opacity = 1;
+			playerOpt = t.target.getAttribute('opt');
+
+			inimigoJogar();
+
+		})
+	}
+
+	function validarVitoria() {
+		let vencedor = document.querySelector('.vencedor');
+
+		if(playerOpt == "papel"){
+			
+	 if(inimigoOpt == "papel"){
+	 	//empate
+	 	vencedor.innerHTML = "O jogo foi empatado!";
+		}else if(inimigoOpt == "tesoura"){
+			//Inimigo ganhou
+			vencedor.innerHTML = "O inimigo ganhou o jogo!";
+		}else if(inimigoOpt == "pedra"){
+			//player ganhou
+			vencedor.innerHTML = "O player ganhou o jogo!";
+		}
+
+	} 
+
+	if(playerOpt == "tesoura"){
+		if(inimigoOpt == "papel"){
+	 	//player ganhou
+	 	vencedor.innerHTML = "O player ganhou o jogo!";
+		}else if(inimigoOpt == "tesoura"){
+			//Empate
+			vencedor.innerHTML = "O jogo foi empatado!";
+		}else if(inimigoOpt == "pedra"){
+			//Inimigo ganhou
+				vencedor.innerHTML = "O inimigo ganhou o jogo!";
+}
+
+	}
+
+	if(playerOpt == "pedra"){
+		if(inimigoOpt == "papel"){
+	 	//inimigo ganhou
+	 		vencedor.innerHTML = "O inimigo ganhou o jogo!";
+		}else if(inimigoOpt == "tesoura"){
+			//player ganhou
+			vencedor.innerHTML = "O player ganhou o jogo!";
+		}else if(inimigoOpt == "pedra"){
+			//empate
+			vencedor.innerHTML = "O jogo foi empatado!";
+	
+
+
+
+
+	}
+}
+}
